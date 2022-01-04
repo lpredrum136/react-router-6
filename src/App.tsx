4a627e98bdfa4c7ee21a6b7e9363e96a5e50a288
Henry Nguyen <nguyenhai1591@gmail.com>
2022-01-04 18:18:01 +1000

    Not Found

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Bookkeeper</h1>
-      <nav>
-        <Link to='/invoices'>Invoices</Link> |{' '}
-        <Link to='/expenses'>Expenses</Link>
+      <nav
+        style={{
+          borderBottom: '1px solid',
+          paddingBottom: '1rem'
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{' '}
+        <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Invoices from './pages/Invoices';
-import Expenses from './pages/Expenses';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Invoices from './pages/Invoices'
+import Expenses from './pages/Expenses'
+import NotFound from './pages/NotFound'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='invoices' element={<Invoices />} />
-        <Route path='expenses' element={<Expenses />} />
+        <Route path="/" element={<App />}>
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="expenses" element={<Expenses />} />
+          {/* No other routes match */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
