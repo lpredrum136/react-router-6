6eaa0cee57794510df6ddb108950c70617cd8176
Henry Nguyen <nguyenhai1591@gmail.com>
2022-01-04 22:09:48 +1000

    navigate programmatically

--- src/pages/Invoice.tsx
+++ src/pages/Invoice.tsx
@@ -1,12 +1,18 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { invoices } from '../data/invoices'
 
 const Invoice = () => {
   const params = useParams<{ invoiceId: string }>()
+  const navigate = useNavigate()
 
   const invoice = invoices.find(
     invoice => invoice.id.toString() === params.invoiceId
   )
+
+  useEffect(() => {
+    if (!invoice) navigate('/invoices')
+  }, [invoice])
 
   return (
     <h2 style={{ padding: '1rem' }}>
